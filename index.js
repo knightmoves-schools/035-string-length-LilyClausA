a3e7ed76ea2df6c2698cb2c3e0108d6e8be5883f
LilyClausA <lilcla1230@students.maquoketaschools.org>
2025-10-31 12:45:41 -0500

    Correct quote length and letter extraction logic
    Fix errors in quote length and letter extraction.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
-
-//Add code below
+let quoteLength = quote.quoteLength;
+let twentySecondLetter = quote [22]
+let output = quoteLength [twentySecondLetter]
+  document.getElementById('result').innerHTML = output;
