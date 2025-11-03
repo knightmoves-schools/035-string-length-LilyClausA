8f7390ecada40ce3b991003216244237f21f9344
LilyClausA <lilcla1230@students.maquoketaschools.org>
2025-11-03 14:03:25 -0600

    Fix syntax errors in index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
+
+//Add code below
+
 let quoteLength = quote.quoteLength;
-let twentySecondLetter = quote [22]
-let output = quoteLength [twentySecondLetter]
+
+let twentySecondLetter = quote[22];
+
+let output = quoteLength[twentySecondLetter];
+
   document.getElementById('result').innerHTML = output;
+
